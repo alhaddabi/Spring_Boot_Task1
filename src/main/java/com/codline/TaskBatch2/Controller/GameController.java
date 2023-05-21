9ff73d037e52688bff85ff484c21c516412dc8cf
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-21 16:27:23 +0400

    the get for the Game class is created and the shearch the game by the name is also created but, still some issue working on it

--- src/main/java/com/codline/TaskBatch2/Controller/GameController.java
+++ src/main/java/com/codline/TaskBatch2/Controller/GameController.java
@@ -5,14 +5,14 @@ import com.codline.TaskBatch2.Models.Game;
 import com.codline.TaskBatch2.Models.Leaderboard;
 import com.codline.TaskBatch2.RequestObject.GetGameRequest;
 import com.codline.TaskBatch2.RequestObject.GetLeaderboardRequest;
+import com.codline.TaskBatch2.ResponseObject.GetGameResponse;
 import com.codline.TaskBatch2.Service.GameService;
 import com.codline.TaskBatch2.Service.LeaderboardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class GameController {
@@ -28,14 +28,28 @@ public class GameController {
     }
 
 
+    @RequestMapping("Game/get")
+    public List<Game> getGame ()
+    {
+        return gameService.getGame();
+    }
+
+
+    @GetMapping("Game/get/{sportName}")
+    public GetGameResponse createGAme (@PathVariable Long gameId)
+    {
+        return gameService.getSPortByName(gameId);
+    }
+
+
     public void createGame(GetGameRequest gameRequest)
     {
         Game game = new Game();
         game.setGameDesignation("11 player in every team");
-        game.setSportName("Football");
+        game.setSportName("basketball");
+        game.setCreatedDate(new Date());
         game.setUpdatedDate(new Date());
         game.setIsActive(true);
-        game.setCreatedDate(new Date());
         gameService.saveGame(game);
     }
 }
--- src/main/java/com/codline/TaskBatch2/Service/GameService.java
+++ src/main/java/com/codline/TaskBatch2/Service/GameService.java
@@ -5,19 +5,34 @@ import com.codline.TaskBatch2.Models.Game;
 import com.codline.TaskBatch2.Models.Player;
 import com.codline.TaskBatch2.Repository.GameReprository;
 import com.codline.TaskBatch2.Repository.PlayerRepository;
+import com.codline.TaskBatch2.ResponseObject.GetGameResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
     @Autowired
     GameReprository gameReprository;
 
-    public List<Game> getGame() {return gameReprository.findAll();
+    public List<Game> getGame() {
+        return gameReprository.findAll();
     }
-    public void saveGame(Game game) {gameReprository.save(game); }
 
+    public void saveGame(Game game) {
+        gameReprository.save(game);
+    }
 
+    public GetGameResponse getSPortByName(Long sportId) {
+        Optional<Game> optionalGame = gameReprository.findById(sportId);
+        if (!optionalGame.isEmpty()) {
+            Game game = optionalGame.get();
+            GetGameResponse gameResponse = new GetGameResponse(game.getSportName() , game.getGameDesignation());
+            return gameResponse;
+        } else {
+            return null;
+        }
+    }
 }
